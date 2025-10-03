5e97b281a4e16a6ec57f9e4579135118ed7c19ad
Rediet-546 <rmengistu546@gmail.com>
2025-10-03 21:02:03 +0300

    third

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Register } from './pages/register/register';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import {routes} from './app.routes';
 
-import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+  providers: [provideBrowserGlobalErrorListeners(),
+             provideZoneChangeDetection(),
+             provideRouter(routes),
+             provideHttpClient()
   ]
 };
--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,18 +1,47 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient,HttpClientModule } from '@angular/common/http';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
+  imports: [FormsModule,HttpClientModule,NgIf],
   templateUrl: './register.html',
   styleUrls: ['./register.css']
 })
 export class Register {
+  isLogin = false; // start with 
+  
+  http=inject(HttpClient)
 
-  // ✅ Signal to track which form is visible
-  isLoginFormVisible = signal<boolean>(false);
+  registerObj: any={
 
-  toggleForm() {
-    this.isLoginFormVisible.set(!this.isLoginFormVisible());
+    "fullname":"string",
+    "email":"string",
+    "password":"string",
+    "collegeName":"string",
+    "role":"string"
+
+
+
   }
 
+  toggleForm() {
+    this.isLogin = !this.isLogin;
+  }
+
+  onLoginSubmit(form: any) {
+    console.log('Login submitted:', form.value);
+  }
+
+  onRegisterSubmit(form: any) {
+    console.log('Register submitted:', form.value);
+  }
+
+onRegister(){
+  this.http.post("https://api.freeprojectapi.com/api/ProjectCompetition/register",this.registerObj).subscribe((res:any)=>{})
+
+  
+}
 }
